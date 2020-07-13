fd578882a45881b0f615ee604673ddd416c6451a
Amy <amalia.dambu@yahoo.com>
2020-07-13 22:13:22 +0300

    Problems in installing TSLint package

---
+++ src/app/types/CartType.ts
@@ -0,0 +1,6 @@
+import {ProductListComponent} from "../product-list/product-list.component";
+
+export class Cart {
+  cartId: number;
+  products: ProductListComponent[];
+}
---
+++ src/app/types/ProductListType.ts
@@ -0,0 +1,8 @@
+import {ProductListComponent} from "../product-list/product-list.component";
+import {ProductInfo} from "./productInfo";
+
+export class ProductListType {
+  quantity: number;
+  productInfo: ProductInfo;
+
+}
